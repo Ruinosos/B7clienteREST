e1e752a825fc501bb78ff144d437557fd509792d
pablogodiaz <pablogodiaz+1@gmail.com>
2022-11-21 17:55:12 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Booking from "./pages/Booking";
+import Household from "./pages/Household";
 import { Map } from "./pages/Map";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -13,7 +13,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/booking" element={<Booking />} />
+        <Route path="/household" element={<Household />} />
         <Route path="/map" element={<Map />} />
       </Routes>
     </BrowserRouter>
