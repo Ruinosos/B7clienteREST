dd7b5a9c473c55f4007ec291a0d3572f00aedeca
nicolasZhao1908 <0619971117@uma.es>
2022-11-21 18:04:13 +0100

    Update MapForm.js
    * Added state management for each form component (formData, setFormData)
    
    * Added getCurrentDate()
    
    * Now clicking the submit button shows a spinner while loading
    
    * Added src/api/FetchOpenData.js
    
    * Added src/hooks/useInterval.js

---
+++ src/api/FetchOpenData.js
@@ -0,0 +1,39 @@
+const API_BASE_URL = "http://localhost:8000";
+
+const getBusByID = async (id) => {
+  const res = await fetch(`http://localhost:8000/buses/${id}`);
+  const data = await res.json();
+  return data;
+};
+
+const getRouteTo = async (destination) => {
+  const res = await fetch(
+    `https://nominatim.openstreetmap.org/search.php?q=${destination}&format=jsonv2&countrycodes=es&polygon_geojson=1`
+  );
+  const data = await res.json();
+  const { lat, lon, geojson } = data[0];
+  return { lat: lat, lon: lon, geojson: geojson };
+};
+
+const getCurrentCoords = async () => {
+  return new Promise((resolve, _) =>
+    navigator.geolocation.getCurrentPosition(async ({ coords }) => {
+      resolve(coords);
+    })
+  );
+};
+
+const getNearbyBuses = async () => {
+  const current = await getCurrentCoords();
+  const res = await fetch(
+    `${API_BASE_URL}/buses/search/nearby?lat=${current.latitude}&lon=${current.longitude}`
+  );
+  return res.json();
+};
+
+const getTodayForecast = async () => {
+  const res = await fetch(`${API_BASE_URL}/forecasts/today`);
+  return res.json();
+};
+
+export { getBusByID, getRouteTo, getNearbyBuses, getTodayForecast };
--- src/components/Map/MapForm.js
+++ src/components/Map/MapForm.js
@@ -2,50 +2,106 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
-import ToggleButton from "react-bootstrap/ToggleButton";
+import { getNearbyBuses } from "../../api/FetchOpenData";
+import { useState } from "react";
+import { useInterval } from "../../hooks/useInterval";
 
 export const MapForm = () => {
+  const getCurrentDate = () => {
+    return new Date().toISOString().slice(0, 10);
+  };
+
+  const [formData, setFormData] = useState({
+    addressInput: "",
+    radius: 500,
+    startingDate: getCurrentDate(),
+    endingDate: getCurrentDate(),
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const spinner = () => {
+    return (
+      <div className="spinner-border" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+    );
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    // TODO: Fetch household given the form data (address, startDate, endDate, radius (default to 500m))
+    console.log(await getNearbyBuses());
+    setIsLoading(false);
+  };
+
+  // Fetch bus data every 10s
+  const REFRESH_RATE_MS = 10000;
+
+  useInterval(async () => {
+    // TODO: Print in map
+    console.log(await getNearbyBuses());
+  }, REFRESH_RATE_MS);
+
+  const updateFormData = (event) => {
+    const { name, value } = event.target;
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
   return (
-    <Form className="mw-50">
+    <Form onSubmit={submitHandler}>
       <Row>
         <Form.Group className="mb-4" controlId="formAddress">
           <Form.Label>Dirección</Form.Label>
-          <Form.Control type="text" placeholder="Introduzca una dirección" />
+          <Form.Control
+            type="text"
+            placeholder="Introduzca una dirección"
+            name="addressInput"
+            onChange={updateFormData}
+          />
         </Form.Group>
       </Row>
       <Row>
         <Form.Label>Radio de búsqueda</Form.Label>
-        <ButtonGroup className="mb-2">
-          <ToggleButton
-            type="radio"
-            variant="primary"
-            name="radio"
-            onChange={(e) => console.log(e.currentTarget.value)}
-          >
+        <ButtonGroup className="mb-2" name="radius" onClick={updateFormData}>
+          <Button variant="primary" name="radius" value={500}>
             500m
-          </ToggleButton>
-          <ToggleButton
-            type="radio"
-            variant="primary"
-            name="radio"
-            onChange={(e) => console.log(e.currentTarget.value)}
-          >
+          </Button>
+          <Button variant="primary" name="radius" value={1000}>
             1000m
-          </ToggleButton>
+          </Button>
         </ButtonGroup>
       </Row>
       <Row className="mt-4">
         <Form.Group className="col-6" controlId="formStartDate">
           <Form.Label>Inicio</Form.Label>
-          <Form.Control type="date" placeholder="Inicio" />
+          <Form.Control
+            type="date"
+            placeholder="Inicio"
+            name="startingDate"
+            value={formData.startingDate}
+            onChange={updateFormData}
+          />
         </Form.Group>
 
         <Form.Group className="col-6" controlId="formEndDate">
           <Form.Label>Fin</Form.Label>
-          <Form.Control type="date" placeholder="Fin" />
+          <Form.Control
+            type="date"
+            placeholder="Fin"
+            name="endingDate"
+            value={formData.endingDate}
+            onChange={updateFormData}
+          />
         </Form.Group>
-        <Button className="mt-5 ms-3 col-4" variant="primary" type="submit">
-          Submit
+        <Button className="mt-5 ms-3 w-25 h-25" variant="primary" type="submit">
+          {isLoading ? spinner() : "Submit"}
         </Button>
       </Row>
     </Form>
