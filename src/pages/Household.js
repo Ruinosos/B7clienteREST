60b093a9c9b292634e643fe1ad78f058f8990bc3
VictorMadrona <95759313+VictorMadrona@users.noreply.github.com>
2022-11-22 16:13:17 +0100

    Update Household.js

--- src/pages/Household.js
+++ src/pages/Household.js
@@ -1,7 +1,7 @@
 //import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
-//import Button from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
@@ -16,10 +16,10 @@ export default function Household() {
       <Col xs={4} className="mt-5">
         <Form className="list-group mb-3 d-flex">
           <Row className="list-group-item d-flex justify-content-between lh-sm">
-            <div>
+            
             <Form.Label className="small my-0">Precio por Noche</Form.Label>
-            </div>
-            <span class=" mt-2">$12</span>
+            
+            <Form.Label className=" mt-2">$12</Form.Label>
           </Row>
           <Row className="list-group-item d-flex justify-content-between lh-sm">
           <Col>
@@ -36,6 +36,7 @@ export default function Household() {
             </Col>
           </Row>
           <Row className="list-group-item d-flex lh-sm">
+            <Form.Label className="small">Nº huéspedes</Form.Label>
             <Form.Select className="w-25">
               <option>1</option>
               <option>2</option>
@@ -44,46 +45,19 @@ export default function Household() {
               <option>5</option>
             </Form.Select>
           </Row>
+
+          <div style={{ borderTop: "2px solid #fff ", marginLeft: 20, marginRight: 20 }}></div>
+
+          <Row className="list-group-item d-flex justify-content-end">
+            <Form.Label className="big w-50 justify-content-start">Total:</Form.Label>
+            <Form.Label className="big w-50 justify-content-end">25$</Form.Label>
+          </Row>
+
           <Row className="list-group-item d-flex justify-content-between lh-sm">
-            <div>
-              <h6 class="my-0">Second product</h6>
-              <small class="text-muted">Brief description</small>
-            </div>
-            <span class="text-muted">$8</span>
+          <Button variant="primary" type="submit"> Reservar </Button>
           </Row>
-          <Row className="list-group-item d-flex justify-content-between lh-sm">
-            <div>
-              <h6 class="my-0">Second product</h6>
-              <small class="text-muted">Brief description</small>
-            </div>
-            <span class="text-muted">$8</span>
-          </Row>
-          <Row className="list-group-item d-flex justify-content-between">
-            <span>Total (USD)</span>
-            <strong>$20</strong>
-          </Row>
-        </Form>
-        <Form className="mw-50">
-          <Row>
-            <Form.Group className="mb-4" controlId="pricePerNight">
-              <Form.Label className="">Precio por Noche</Form.Label>
-              <div>precio</div>
-            </Form.Group>
-          </Row>
-          <Row className="mt-4 mw-50">
-            <Col>
-              <Form.Group className="mt-4 mw-25" controlId="startDate">
-                <Form.Label>Fecha Inicio</Form.Label>
-                <Form.Control type="date" placeholder="inicio"/>
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group className="mt-4 mw-25" controlId="endDate">
-                <Form.Label>Fecha Fin</Form.Label>
-                <Form.Control type="date" placeholder="final"/>
-              </Form.Group>
-            </Col>
-          </Row>
+         
+          
         </Form>
       </Col>
     </Row>
