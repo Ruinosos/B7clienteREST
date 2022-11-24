ef5beb9539e02de48aba27ab006a1d7894630cc2
AlfonsoGonzalezOrtiz <80314412+AlfonsoGonzalezOrtiz@users.noreply.github.com>
2022-11-24 18:32:26 +0100

    Cambios en el album de anuncios

--- src/components/Album/Album.js
+++ src/components/Album/Album.js
@@ -1,22 +1,21 @@
-import { CardGroup, Container,Row} from 'react-bootstrap';
-import CardComponent from '../Card/Card';
-
+import { CardGroup, Container, Row} from "react-bootstrap";
+import CardComponent from "../Card/Card";
 
 export const AlbumComponent = () => {
-    return (
-        <CardGroup class="py-5 bg-light">
-            <Container>
-                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                        <CardComponent></CardComponent>
-                        <CardComponent></CardComponent>
-                        <CardComponent></CardComponent>
-                        <CardComponent></CardComponent>
-                        <CardComponent></CardComponent>
-                        <CardComponent></CardComponent>
-                </div>
-            </Container>
-        </CardGroup>
-    );
-}
+  return (
+    <CardGroup className="py-5 bg-white">
+      <Container>
+        <Row className="row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          <CardComponent></CardComponent>
+          <CardComponent></CardComponent>
+          <CardComponent></CardComponent>
+          <CardComponent></CardComponent>
+          <CardComponent></CardComponent>
+          <CardComponent></CardComponent>
+        </Row>
+      </Container>
+    </CardGroup>
+  );
+};
 
 export default AlbumComponent;
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,22 +1,23 @@
-import { Button, Card, Col } from 'react-bootstrap';
+import { Button, ButtonGroup, Card, Col, Container} from 'react-bootstrap';
 
 function CardComponent() {
     return (
         <Col>
-            <div class="card shadow-sm">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <Card>
+                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="https://e00-expansion.uecdn.es/assets/multimedia/imagenes/2021/04/16/16185679347539.jpg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
 
-                <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                        </div>
-                        <small class="text-muted">9 mins</small>
+                <Card.Body>
+                    <Card.Text>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</Card.Text>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <ButtonGroup>
+                            <button type="button" className="btn btn-sm btn-primary">View</button>
+                            <button type="button" className="btn btn-sm btn-success">Edit</button>
+                            <button type="button" className="btn btn-sm btn-danger">Delete</button>
+                        </ButtonGroup>
+                        <div class="text-muted">9 mins</div>
                     </div>
-                </div>
-            </div>
+                </Card.Body>
+            </Card>
         </Col>
     );
 }
