ef651915fe13e14091ce70b58422cb620da5edf9
pablogodiaz <pablogodiaz+1@gmail.com>
2022-12-02 18:43:45 +0100

    Created CreateHousehold.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Household from "./pages/Household";
+import CreateHousehold from "./pages/CreateHousehold";
 import { Map } from "./pages/Map";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -15,6 +16,7 @@ root.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/household/:id" element={<Household />} />
+        <Route path="/createHousehold" element={<CreateHousehold />} />
         <Route path="/map" element={<Map />} />
       </Routes>
     </BrowserRouter>
