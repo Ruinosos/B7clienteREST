90be467886358900ce7d1f0303424f7d67c4f9fd
VictorMadrona <95759313+VictorMadrona@users.noreply.github.com>
2022-12-02 18:27:55 +0100

    Create MyBookings.js

---
+++ src/pages/MyBookings.js
@@ -0,0 +1,10 @@
+import { NavbarComponent } from "../components/Navbar/Navbar";
+
+
+export default function MyBookings(){
+    return(
+        <>
+            <NavbarComponent />
+        </>
+        );
+};
