050614207e1c42c1b09ba874334c810b83dd88cd
nicolasZhao1908 <0619971117@uma.es>
2022-12-23 16:48:53 +0100

    refactor google oauth2

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,33 +3,33 @@ import { Nav, Navbar, Container } from "react-bootstrap";
 export const NavbarComponent = ({ auth: Auth }) => {
   let username = undefined;
 
-    try{
-        username = JSON.parse(localStorage.getItem('profile')).name;
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    username = JSON.parse(localStorage.getItem("profile")).name;
+  } catch (e) {
+    console.log(e);
+  }
   return (
-    <Navbar bg="light">
-      <Container className="d-flex justify-content-start align-items-center mx-1">
+    <Navbar bg='light'>
+      <Container className='d-flex justify-content-start align-items-center mx-1'>
         <Navbar.Brand
-          href="/"
-          className="mx-0 d-flex justify-content-center align-items-center"
+          href='/'
+          className='mx-0 d-flex justify-content-center align-items-center'
         >
           <img
-            src="https://i.imgur.com/P7KGTpL.png"
-            alt="Brand"
+            src='https://i.imgur.com/P7KGTpL.png'
+            alt='Brand'
             style={{ height: "40px", width: "50px" }}
           ></img>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Nav>
-          <Nav.Link href="/createHousehold">Crear Anuncio</Nav.Link>
+          <Nav.Link href='/createHousehold'>Crear Anuncio</Nav.Link>
           <Nav.Link href={`/myhouseholds/${username}`}>Mis anuncios</Nav.Link>
           <Nav.Link href={`/mybookings/${username}`}>Mis reservas</Nav.Link>
-          <Nav.Link href="/map">Buscar Ubicación</Nav.Link>
+          <Nav.Link href='/map'>Buscar Ubicación</Nav.Link>
         </Nav>
       </Container>
-      <div className="ms-auto me-5">
+      <div className='ms-auto me-5'>
         <Auth />
       </div>
     </Navbar>
