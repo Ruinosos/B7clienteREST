f467c5abf8c1fe60094bdb57eb86844d3f65582e
VictorMadrona <95759313+VictorMadrona@users.noreply.github.com>
2022-12-31 15:12:41 +0100

    refresh page when creating comment

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -17,6 +17,10 @@ import { useEffect, useState } from "react";
 
 export const Comment = ({ idHousehold }) => { 
 
+    function refreshPage() {
+        window.location.reload(false);
+      }
+
     const username = undefined;
     const email = undefined;
     const photo = undefined;
@@ -98,6 +102,7 @@ export const Comment = ({ idHousehold }) => {
         }
         console.log(jsonData);
         createComment(jsonData);
+        refreshPage();
   
     };
 
